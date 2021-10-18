30d4a97ecc0d32afa7fe2340c084550a6e8a517b
adelliott92 <andrewdavidelliott@outlook.com>
2021-10-18 19:03:31 +0100

    🔧 chore: create tables in models.py for tower_store app

--- tower_store/models.py
+++ tower_store/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class storeCategory(models.Model):
+    categoryName = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse("store:category_list", args=[self.slug])
+    
+
+    def __str__(self):
+        return self.categoryName
+
+
+class storeProduct(models.Model):
+    category = models.ForeignKey(storeCategory, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    productTitle = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='Tower Records Admin')
+    productDescription = models.TextField(blank=True)
+    images = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=255)
+    productPrice = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
+    createdOn = models.DateTimeField(auto_now_add=True)
+    updatedOn = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-createdOn',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+    
+    def __str__(self):
+        return self.productTitle
