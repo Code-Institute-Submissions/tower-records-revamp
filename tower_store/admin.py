7fce18314a13bb0d324693efb4af92d3a9816853
adelliott92 <andrewdavidelliott@outlook.com>
2021-10-24 12:01:21 +0100

    🔧 chore: configure administration page for tower_store app

--- tower_store/admin.py
+++ tower_store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import storeCategory, storeProduct
 
-# Register your models here.
+@admin.register(storeCategory)
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ['categoryName', 'slug']
+    prepopulated_fields = {'slug': ('categoryName',)}
+
+
+@admin.register(storeProduct)
+class productAdmin(admin.ModelAdmin):
+    list_display = ['productTitle', 'author', 'slug', 'productPrice', 'in_stock', 'createdOn', 'updatedOn']
+    list_filter = ['in_stock', 'active']
+    list_editable = ['productPrice', 'in_stock']
+    prepopulated_fields = {'slug': ('productTitle',)}
